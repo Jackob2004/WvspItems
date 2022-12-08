f0b5ff40c5b2c70b74b4774ee4c4e1ca855ba1db
Jackob2004 <zjakub2004@gmail.com>
2022-12-08 17:32:10 +0100

    Items atomowka

--- src/main/java/me/shadow_warrior_/wvspitems/Listeners/AtomowkaListener.java
+++ src/main/java/me/shadow_warrior_/wvspitems/Listeners/AtomowkaListener.java
@@ -1,6 +1,7 @@
 package me.shadow_warrior_.wvspitems.Listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -16,11 +17,12 @@ public class AtomowkaListener implements Listener {
         Player player = e.getPlayer();
         if(e.getPlayer().getItemInHand().getItemMeta() == null || e.getPlayer().getItemInHand().getItemMeta().getDisplayName() == null || (e.getPlayer().getItemInHand() == null)) return;
         if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getPlayer().getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.RED + "" + ChatColor.BOLD + "ATOMOWKA")){
+            player.playSound(player.getLocation(), Sound.COW_HURT,1,1);
             e.getPlayer().getWorld().createExplosion(player.getLocation(),1);
             double x = 5;
             double y = 5;
-            double z = 5;
-            int a = 20;
+            double z = 5; //5,10,15,20
+            int a = 25; // 25,15,10,5
             for (int i = 0;i<=3;i++) {
                 for (Entity entity : player.getNearbyEntities(x, y, z)) {
                     if (entity.getType().equals(EntityType.PLAYER)) {
@@ -31,7 +33,7 @@ public class AtomowkaListener implements Listener {
                 x = x+5;
                 y = y+5;
                 z = z+5;
-                a = a-4;
+                a = a-5;
             }
             player.setHealth(0);
         }
